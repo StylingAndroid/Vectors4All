47de3a5aa425453e4aaaa34eebed3c0d11759abd
Mark Allison <mark@stylingandroid.com>
2016-02-27 12:33:17 +0000

    Updates to latest build tools, and makes necessary changes to use VectorDrawableCompat

--- app/src/main/java/com/stylingandroid/vectors4all/MainActivity.java
+++ app/src/main/java/com/stylingandroid/vectors4all/MainActivity.java
@@ -1,7 +1,7 @@
 package com.stylingandroid.vectors4all;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
